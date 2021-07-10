45b0cbbb37ffe98f229402bcd81dee0ecb8df5b5
Nikolay Gutorov <ngutorov@gmail.com>
2021-07-10 00:11:33 -0700

    Initial Commit

---
+++ Global-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Global-List
+//
+//  Created by Nikolay Gutorov on 7/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
